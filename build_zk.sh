c2d461dd2a7bda733949ae7a811c9bb4294d94eb
yunnysunny <yunnysunny@gmail.com>
2022-09-02 18:40:54 +0800

    chore: rename dockerfile filename

--- build_zk.sh
+++ build_zk.sh
@@ -4,7 +4,7 @@ set -e
 ZOOKEEPER_VERSION=3.7.1
 
 docker pull ubuntu:22.04
-docker build . -f zk.Dockerfile -t yunnysunny/zookeeper:${ZOOKEEPER_VERSION} \
+docker build . -f Dockerfile -t yunnysunny/zookeeper:${ZOOKEEPER_VERSION} \
 --build-arg ZOOKEEPER_VERSION=${ZOOKEEPER_VERSION}
 if [ "$NEED_PUSH" = "1" ] ; then
     docker push yunnysunny/zookeeper:${ZOOKEEPER_VERSION}
